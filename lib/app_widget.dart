58ada545f592b80255dc05863be0d5d61f2289ad
ocostalucas <olucascosta98@gmail.com>
2020-10-14 22:06:46 -0300

    Initial commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Container());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/app_widget.dart';
+
+void main() {
+  runApp(AppWidget());
+}
